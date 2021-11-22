11beed955f2f274a7799b689f414bf6664661862
Michael Gaudreau <gaudreaum01@gmail.com>
2021-11-22 13:58:30 -0500

    Add router implementation for "/" path and "/search" path using placeholders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      Toolbar Placeholder
+      <Routes>
+        <Route path="/" element={<Placeholder msg="Home" />} />
+        <Route path="/search" element={<Placeholder msg="Search" />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+function Placeholder({ msg }) {
+  return (
+    <>
+      <h1>{msg}</h1>
+    </>
+  );
+}
+
+Placeholder.propTypes = {
+  msg: PropTypes.string.isRequired,
+};
