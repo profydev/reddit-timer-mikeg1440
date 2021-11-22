a98afad249ff37484bb5ff950648d131e7fbabf2
Michael Gaudreau <gaudreaum01@gmail.com>
2021-11-22 16:05:34 -0500

    Create a Footer component with links

---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import Logo from './Logo';
+
+export default function Footer() {
+  return (
+    <StyledFooter>
+      <StyledLink to="https://profy.dev/employers">
+        profy.dev
+      </StyledLink>
+
+      <Link to="/">
+        <Logo />
+      </Link>
+
+      <StyledLink to="/terms">
+        Terms & Privacy
+      </StyledLink>
+    </StyledFooter>
+  );
+}
+
+const StyledFooter = styled.footer`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 1rem 5rem;
+`;
+
+const StyledLink = styled(Link)`
+  font-family: 'Montserrat';
+  color: gray;
+  text-decoration: none;
+  :visited {
+    color: gray;
+  }
+`;
