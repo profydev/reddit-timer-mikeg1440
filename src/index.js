d1cfe5b115513c1b0f05ed478a3093139c2af65f
Michael Gaudreau <gaudreaum01@gmail.com>
2021-11-22 14:24:29 -0500

    Add webfontloader implementation and get 'Bitter' and 'Montserrat' fonts loaded

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './index.css';
+import WebFont from 'webfontloader';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+
+WebFont.load({
+  google: {
+    families: ['Bitter', 'Montserrat'],
+  },
+});
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
