64c1d9d6639407d7a72e6810706c6bc267753ee8
Michael Gaudreau <gaudreaum01@gmail.com>
2021-11-22 14:24:46 -0500

    Add a global style to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle({
+  body: {
+    fontFamily: 'Bitter',
+  },
+});
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       Toolbar Placeholder
       <Routes>
         <Route path="/" element={<Placeholder msg="Home" />} />
