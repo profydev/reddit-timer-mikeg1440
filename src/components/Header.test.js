548946a9e6ef90a7d5f9c58175d91bb64ec17d7c
Michael Gaudreau <gaudreaum01@gmail.com>
2021-11-27 00:13:46 -0500

    Fix linting error in Header tests

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { render as rtlRender, screen, getByRole } from '@testing-library/react';
+import { render as rtlRender, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './Header';
 
