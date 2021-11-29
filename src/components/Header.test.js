af5fc231f4a9f199d69f351469e4dd321c43942d
Michael Gaudreau <gaudreaum01@gmail.com>
2021-11-29 13:27:24 -0500

    Fix Header component tests

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,55 +1,77 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { render as rtlRender, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import Header from './Header';
 
-const render = (ui, { route = '/' } = {}) => {
-  window.history.pushState({}, 'Test Page', route);
+// const render = (ui, { route = '/' } = {}) => {
+//   window.history.pushState({}, 'Test Page', route);
 
-  return rtlRender(ui, { wrapper: Router });
+//   // return rtlRender(ui, { wrapper: Router });
+//   const router = rtlRender(ui, { wrapper: Router });
+//   return rtlRender(router, { wrapper: ThemeProvider });
+// };
+
+const testTheme = {
+  mode: 'light',
+  text: {
+    primary: 'black',
+    secondary: 'gray',
+  },
+  background: {
+    primary: 'white',
+    secondary: 'lightgray',
+  },
 };
+
+const testRender = () => (
+  <ThemeProvider theme={testTheme}>
+    <Router>
+      <Header />
+    </Router>
+  </ThemeProvider>
+);
 
 describe('Header Component', () => {
   test('It renders About link', () => {
-    render(<Header />);
+    render(testRender());
     expect(screen.getByText(/About/g)).toBeInTheDocument();
   });
 
   test('It renders Search link', () => {
-    render(<Header />);
+    render(testRender());
     expect(screen.getByText(/Search/)).toBeInTheDocument();
   });
 
   test('It renders "How it works" link', () => {
-    render(<Header />);
+    render(testRender());
     expect(screen.getByText(/How it works/)).toBeInTheDocument();
   });
 
   test('It renders the Logo image', () => {
-    render(<Header />);
+    render(testRender());
     expect(screen.findByRole('elements/logo')).toBeTruthy();
-    screen.debug();
   });
 
   describe('Header Nav Links', () => {
     test('It renders logo inside a "a" tag that points to "/"', () => {
-      render(<Header />);
+      render(testRender());
       // expect(screen.getByRole('link/').getAttribute('href')).toEqual('/');
       expect(screen.getByRole('link', { name: '' }).getAttribute('href')).toEqual('/');
     });
 
     test('About link is a hashlink', () => {
-      render(<Header />);
+      render(testRender());
       expect(screen.getByRole('link', { name: 'About' })).toBeTruthy();
     });
 
     test('How it works link is a hash link', () => {
-      render(<Header />);
+      render(testRender());
       expect(screen.getByRole('link', { name: 'How it works' })).toBeTruthy();
     });
 
     test('Search link is not a hashlink', () => {
-      render(<Header />);
+      render(testRender());
       expect(screen.getByRole('link', { name: 'Search' }).getAttribute('href')).toEqual('/search?value=javascript');
     });
   });
